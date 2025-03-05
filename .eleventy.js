3f6974df583eb737e2bae24a454d1ae49e925f7f
Johann M <johann.misterio@gmail.com>
2025-03-04 22:23:25 -0500

    adjusments for using github secrets for resume data

--- .eleventy.js
+++ .eleventy.js
@@ -7,36 +7,52 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
 
     // Load JSON site data
-    eleventyConfig.addGlobalData('site', require('./src/_data/site.json'));
+    eleventyConfig.addGlobalData("site", require("./src/_data/site.json"));
 
     // Load YAML data
-    eleventyConfig.addDataExtension('yaml', contents => yaml.load(contents));
-    eleventyConfig.addDataExtension('yml', contents => yaml.load(contents));
+    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+    eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
 
     // Custom filters
-    eleventyConfig.addFilter('formatDate', (date, format) => {
-        return dayjs(date).format(format);
-    });
-
-    eleventyConfig.addFilter('objtype', obj => typeof obj);
+    eleventyConfig.addFilter("formatDate", (date, format) => dayjs(date).format(format));
+    eleventyConfig.addFilter("objtype", (obj) => typeof obj);
     eleventyConfig.addGlobalData("buildDate", new Date().toISOString());
 
-    // Load resume YAML manually and make it global
-    const resumePath = path.join(__dirname, 'src/_data/example_resume.yml');
-    if (fs.existsSync(resumePath)) {
-        const resumeData = yaml.load(fs.readFileSync(resumePath, 'utf8'));
-        eleventyConfig.addGlobalData('resume', resumeData);
+    // Default to example_resume.yml (local development)
+    let resumePath = path.join(__dirname, "src/_data/example_resume.yml");
+    let resumeData;
+
+    if (process.env.CI && process.env.RESUME) {
+        // In GitHub Actions, prefer the RESUME secret if available
+        try {
+            resumeData = yaml.load(process.env.RESUME);
+            console.log("Using RESUME secret from GitHub Actions.");
+        } catch (error) {
+            console.error("Failed to parse RESUME environment variable as YAML:", error);
+            process.exit(1); // Stop build if malformed
+        }
+    } else {
+        // Otherwise, use the local example_resume.yml
+        if (fs.existsSync(resumePath)) {
+            resumeData = yaml.load(fs.readFileSync(resumePath, "utf8"));
+            console.log("Using example_resume.yml (local development).");
+        } else {
+            console.error("No resume data found!");
+            process.exit(1); // Stop build if no data available
+        }
     }
+
+    eleventyConfig.addGlobalData("resume", resumeData);
 
     return {
         dir: {
-            input: 'src',
-            includes: '_includes',
-            output: '_site',
+            input: "src",
+            includes: "_includes",
+            output: "_site",
         },
-        templateFormats: ['md', 'njk', 'html'],
-        markdownTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
-        dataTemplateEngine: 'njk'
+        templateFormats: ["md", "njk", "html"],
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
     };
 };
